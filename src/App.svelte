f6d05dd343e98ad1f4d4167c9fc6ec062ed1d9e3
Bryan Gordon <bryanbge92@gmail.com>
2025-02-19 21:50:26 -0500

    Update app for weather app

--- src/App.svelte
+++ src/App.svelte
@@ -2,58 +2,22 @@
   // import Counter from './lib/Counter.svelte';
   // import Saludo from './lib/Saludo.svelte';
   // let cont = 0
-    import Message from './lib/Message.svelte';
+  // import Message from './lib/Message.svelte';
 
-    let aux = false 
-    const handleToggle = () => {
-      aux = !aux
-    }
+    import Weather from './lib/Weather.svelte';
 
 </script>
 
 <main>
-  {#if aux}
-    <Message />
-    {:else} 
-      <div>
-        <h1>No es una alerta</h1>
-      </div>
-  {/if}
-
-  <button on:click={handleToggle}>Hidden/Show</button>
+  <header>
+    <h1>Weather App</h1>
+  </header>
+  <Weather />
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-
-  span {
-    margin-right: 10px;
-  }
-
-  button {
-    margin-inline: 10px;
-  }
-
-  .positive {
-    color: green;
-  }
-
-  .negative {
-    color: crimson;
+  h1 {
+    text-align: center;
   }
 
 </style>
