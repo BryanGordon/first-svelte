d2a37f1a912974a020f9de7f6d3812ebbd9081cc
Bryan Gordon <bryanbge92@gmail.com>
2025-02-20 22:49:00 -0500

    Update component

--- src/lib/Weather.svelte
+++ src/lib/Weather.svelte
@@ -1,29 +1,67 @@
 <script lang="ts">
+  const key = import.meta.env.VITE_API_KEY_WEATHER
+  let cityName = ''
+  let emptyError = 'someting goes wrong :('
 
-  const getWeatherData = async () => {
-    const res = await fetch('https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}')
-    const data = res.json
+  const getWeatherData = async (cityName: string = 'Madrid') => {
+    try {
+      if (cityName.trim() === '') {
+        emptyError = 'Invalid city'
+        throw new Error(emptyError)
+      }
+      return await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${key}`)
+      .then(res => res.json())
+    } 
+    catch (error) {
+      throw new Error(emptyError)
+    }
   }
+  
+  let promise = getWeatherData()
 
+  const searchCity = (e: Event) => {
+    e.preventDefault()
+    promise = getWeatherData(cityName)
+  }
 </script>
 
 <section>
-  <header>
-    <h2>Section Weather</h2>
-  </header>
 
-  <article>
-    <div>
-      <h1>20 ºC</h1>
-      <h4>City: <span>Madrid, ES</span></h4>
-      <h4>Weather: <span>Cloudy</span></h4>
-    </div>
-
-    <div>
-      <h4>Maximun Temperature:<span>23 ºC</span></h4>
-      <h4>Minimium Temperature:<span>19 ºC</span></h4>
-    </div>
+  <article class="searching">
+    <label for="search">Search by city: </label>
+    <input
+      bind:value={cityName}
+      id="search"
+      type="text"
+      placeholder="Washington"
+      >
+    <button type="submit" on:click={(e) => searchCity(e)}>Search</button>
   </article>
+
+  {#await promise}
+    Loading ...
+  {:then data}
+    {#if data.cod === '404'}
+      <h3>City not found</h3>
+      {:else}
+      <article>
+        <div>
+          <img src={`https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`} alt={`current weather in ${data.name}`}>
+          <h1>{data.main.temp}ºC</h1>
+          <h4>City: {data.name}, {data.sys.country}</h4>
+          <h4>Weather: {data.weather[0].description}</h4>
+        </div>
+  
+        <div>
+          <h4>Maximun Temperature: {data.main.temp_max}ºC</h4>
+          <h4>Minimium Temperature: {data.main.temp_min}ºC</h4>
+        </div>
+      </article>
+    {/if}
+  {:catch error}
+    <h3>{error}</h3>
+  {/await}
+
 </section>
 
 <style>
